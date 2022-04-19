f7209ff6a77f89784fed4b36193cd200a43398d6
chuckiemaru <cmendietacruz@hotmail.com>
2022-04-19 11:29:45 -0600

    Logramos hacer que el formulario envíe emails a trimetriastudio@gmail.com

--- main.js
+++ main.js
@@ -3,26 +3,22 @@ ScrollReveal().reveal('.contenedor_formulario', {delay: 500});
 ScrollReveal().reveal('.Proyects', {delay: 500});
 ScrollReveal().reveal('footer', {delay: 500});
 
-/**var inputs = document.getElementsByClassName('Formulario_Contacto');
-
-for (var i = 1; i <= inputs.length; i++){
-    inputs[i].addEventListener('keyup', function(){
-        if(this.value.length>=1) {
-            this.nextElementSibling.classList.add('fijar');
-        }
-    })
-}**/
-
 const $form = document.querySelector('#form')
-const $buttonMailto = document.querySelector('#trucazo')
 
 $form.addEventListener('submit', handleSubmit)
 
-
-function handleSubmit(event) {
-    event.preventDefault()
-    const form = new FormData(this)
-    console.log(form.get('name'))
-    $buttonMailto.setAttribute('href', `mailto:trimetriastudio@gmail.com?subject=${form.get('name')}{form.get('email')}&body=${form.get('message')}`)
-    $buttonMailto.click()
+async function handleSubmit(event){
+  event.preventDefault()  
+  const form= new FormData(this)
+  const response= await fetch(this.action, {
+      method: this.method,
+      body: form,
+      header: {
+          'Accept': 'application/json'
+      }
+  })
+  if (response.ok){
+        this.reset()
+        alert('Gracias por contactarnos, te escribiremos pronto.')
+  }
 }
