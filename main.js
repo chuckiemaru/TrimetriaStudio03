6c5ca893a3a1d4c1be5fb577ae4094640681ec33
chuckiemaru <cmendietacruz@hotmail.com>
2022-04-18 20:49:14 -0600

    Finalizada primera etapa de HTML y CSS

---
+++ main.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal('.Proyectos_Presentacion');
+ScrollReveal().reveal('.contenedor_formulario', {delay: 500});
+ScrollReveal().reveal('.Proyects', {delay: 500});
+ScrollReveal().reveal('footer', {delay: 500});
